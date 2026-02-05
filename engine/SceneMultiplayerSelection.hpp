4eb36bf808941db34082e2e4232094102355cb5e
Leminsk <lemosreis.matheus@gmail.com>
2026-02-05 21:13:00 +0000

    add headers and buttons (no function yet) to users ip table

--- engine/SceneMultiplayerSelection.hpp
+++ engine/SceneMultiplayerSelection.hpp
@@ -17,8 +17,10 @@ std::vector<Entity*>& pr_ui_elements = Game::manager->getGroup(groupPriorityUI);
 std::vector<Entity*>&    ui_elements = Game::manager->getGroup(groupUI);
 std::vector<Entity*>& bg_ui_elements = Game::manager->getGroup(groupBackgroundUI);
 SDL_Event* event;
-Entity* table_users_column = nullptr;
-Entity* table_ips_column = nullptr;
+Entity* table_header_users = nullptr;
+Entity* table_header_ips = nullptr;
+Entity* table_column_users = nullptr;
+Entity* table_column_ips = nullptr;
 
 std::ifstream config_file;
 nlohmann::json config_json;
@@ -31,6 +33,7 @@ void goBack() {
 void setUsersIpTable() {
     const SDL_Color background_color = {  20,  20, 100, SDL_ALPHA_OPAQUE };
     const SDL_Color border_color     = { 230, 210, 190, SDL_ALPHA_OPAQUE };
+    std::string header_label = "Username";
 
     std::vector<std::string> users = {};
     std::vector<std::string> ips = {};
@@ -40,34 +43,46 @@ void setUsersIpTable() {
         ips.push_back(ip);
         if(user.size() > max_size_user) { max_size_user = user.size(); }
     }
+    if(header_label.size() > max_size_user) { max_size_user = header_label.size(); }
 
     for(std::string& user : users) {
         int to_pad = max_size_user - user.size();
         std::string padding = "";
         for(int i=0; i<to_pad; ++i) { padding += ' '; }
         user = padding + user;
-        std::cout << "user:"<<user << '\n';
     }
-    for(std::string& ip : ips) {
-        std::cout << "ip:" << ip << '\n';
+    int to_pad = max_size_user - header_label.size();
+    std::string padding = "";
+    for(int i=0; i<to_pad; ++i) { padding += ' '; }
+    header_label = padding + header_label;
+
+    if(this->table_column_users != nullptr) {
+        this->table_column_users->destroy();
+        this->table_column_ips->destroy();
+        this->table_column_users = nullptr;
+        this->table_column_ips = nullptr;
     }
 
-    if(this->table_users_column != nullptr) {
-        this->table_users_column->destroy();
-        this->table_ips_column->destroy();
-        this->table_users_column = nullptr;
-        this->table_ips_column = nullptr;
-    }
-
-    this->table_users_column = &createUIMultilineText(
-        "users_column", users,
+    this->table_header_users = &createUIMultilineText(
+        "table_headers_users", { header_label },
         50, 50,
         Game::default_text_color, background_color, border_color
     );
-    TextBoxComponent& left_column = this->table_users_column->getComponent<TextBoxComponent>();
-    this->table_ips_column = &createUIMultilineText(
+    TextBoxComponent& users_header = this->table_header_users->getComponent<TextBoxComponent>();
+    this->table_header_ips = &createUIMultilineText(
+        "table_headers_users", { "IP" },
+        users_header.x + users_header.w - users_header.border_thickness, users_header.y,
+        Game::default_text_color, background_color, border_color
+    );
+
+    this->table_column_users = &createUIMultilineText(
+        "users_column", users,
+        users_header.x, users_header.y + users_header.h,
+        Game::default_text_color, background_color, border_color
+    );
+    this->table_column_ips = &createUIMultilineText(
         "ips_column", ips,
-        left_column.x + left_column.w - left_column.border_thickness, left_column.y,
+        this->table_header_ips->getComponent<TextBoxComponent>().x, users_header.y + users_header.h,
         Game::default_text_color, background_color, border_color
     );
 }
@@ -89,6 +104,27 @@ void setScene(Mix_Chunk*& sound_b, TextComponent* fps, SceneType parent) {
     const SDL_Color border_color     = { 230, 210, 190, SDL_ALPHA_OPAQUE };
 
     setUsersIpTable();
+
+    TextBoxComponent& t_header_ips = this->table_header_ips->getComponent<TextBoxComponent>();
+    Entity* add_button = &createUIButton(
+        "button_add_user", 
+        " + ",
+        t_header_ips.x + t_header_ips.w + 4, t_header_ips.y,
+        COLORS_GREEN, COLORS_BLACK, border_color,
+        [this](TextBoxComponent& self) {
+            std::cout << "Pressed +\n";
+        }
+    );
+    TextBoxComponent& t_add_button = add_button->getComponent<TextBoxComponent>();
+    createUIButton(
+        "button_delete_user", 
+        " - ",
+        t_add_button.x + t_add_button.w + 4, t_add_button.y,
+        COLORS_RED, COLORS_BLACK, border_color,
+        [this](TextBoxComponent& self) {
+            std::cout << "Pressed -\n";
+        }
+    );
     
     createUIButton(
         "button_back", 
